562798ce52d93ee4156d1d431e75499ad348c696
haresh38 <98345441+haresh38@users.noreply.github.com>
2024-06-16 23:16:59 -0400

    Update CIpherBreak.py

--- CIpherBreak.py
+++ CIpherBreak.py
@@ -1,1 +1,17 @@
+#Haresh Shiwcharan 
+#Assignment 4: Break the Cipher using frequency Analysis
 
+from collections import Counter
+
+# First I want to prompt the user for Cipher Text
+ciphertext = input("Enter the ciphertext: ").upper()
+
+# Step 2: Frequency Analysis
+# Convert the ciphertext to a list of characters
+letters = list(ciphertext)
+
+# Count the frequency of each letter
+frequency = Counter(letters)
+
+# Initialize an empty string for the deciphered text
+deciphered_text = ""
