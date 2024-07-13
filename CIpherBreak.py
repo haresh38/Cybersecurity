2a93083dabf75bd0d4f20dfb0962774b59a1f3f4
Omoze Oyarebu <83746252+Moze-Code@users.noreply.github.com>
2024-07-12 21:54:54 -0400

    Update CIpherBreak.py
    Current working solution.
    
    Flaws right now are:
    a)The one-to-one mapping is not very accurate.
    b)Because of the previously mentioned problem, it need a lot of manually engineering from the user

--- CIpherBreak.py
+++ CIpherBreak.py
@@ -7,10 +7,11 @@ from collections import Counter
 
 # Step 1: Prompt the user for Cipher Text
 ciphertext = input("Enter the ciphertext: ").upper()
+ciphertext2 = ciphertext.replace(" ", "")  # removes spaces
 
 # Step 2: Frequency Analysis
 # Convert the ciphertext to a list of characters
-letters = list(ciphertext)
+letters = list(ciphertext2)
 
 # Count the frequency of each letter
 frequency = Counter(letters)
@@ -59,7 +60,40 @@ english_letter_frequency = {
     'Z': 0.07
 }
 
+# Create a mapping based on sorted frequencies
+sorted_ciphertext_frequency = [item[0] for item in frequency.most_common()]
+sorted_english_frequency = sorted(english_letter_frequency.keys(), key=lambda x: english_letter_frequency[x], reverse=True)
 
+# maps the ciphertext most frequent letters with the standard english letters most frequent letters(one-to-one)
+mapping = dict(zip(sorted_ciphertext_frequency, sorted_english_frequency))
+#print("\nMapped:\n", mapping)
 
-# Initialize an empty string for the deciphered text
-deciphered_text = ""
+# Function to decrypt the ciphertext
+def decrypt(ciphertext, mapping):
+    deciphered_plaintext = ""
+
+    print("\n\nOriginal Cipher Text:\n")
+    print(ciphertext)
+
+    for char in ciphertext:
+        if char in mapping:
+            deciphered_plaintext += mapping[char]
+        else:
+            deciphered_plaintext += char  # If the character is not in mapping(spaces)
+
+    print("\nDeciphered Text:\n", deciphered_plaintext)
+
+    # Ask user if they want to modify the decrypted text
+    modify_text = input("\nWould you like to modify the deciphered text? (y/n): ").lower()
+    if modify_text == 'y':
+        while True:
+            find_char = input("Enter the character you wish to replace (or 'exit' to finish): ")
+            if find_char == 'exit':
+                break
+            replace_char = input("Replace with: ")
+            deciphered_plaintext = deciphered_plaintext.replace(find_char, replace_char)
+
+            print("\nModified Deciphered Text:\n", deciphered_plaintext)
+
+# Call the decryption function
+decrypt(ciphertext, mapping)
